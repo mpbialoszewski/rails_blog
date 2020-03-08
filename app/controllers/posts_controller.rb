f468c5dccd8cc79e0026055e96861d25aad6b6ed
Mariusz <mab152@aber.ac.uk>
2020-03-08 20:51:41 +0000

    Finishing CR from CRUD

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,27 @@
 class PostsController < ApplicationController
-    def index 
-    
-    end
-    def  new
-       
-    end
-    def create
-        render plain: params[:post].inspect
+def index
+    @posts = Post.all
 
-    end
-    
+end
+
+def show
+    @post = Post.find(params[:id])
+end
+
+def new
+
+end
+
+
+def create
+    @post = Post.new(post_params)
+
+    @post.save
+    redirect_to @post
+end
+
+private def post_params
+    params.require(:post).permit(:title, :body)
+end
+
 end
