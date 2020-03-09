b269910205cee5b94ea36cea7bb72d638fc28db3
Mariusz <mab152@aber.ac.uk>
2020-03-09 22:21:39 +0000

    Complete CRUD working
    :)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,18 +1,18 @@
 class PostsController < ApplicationController
+#Home section
 def index
     @posts = Post.all
-
 end
-
+#Display 
 def show
     @post = Post.find(params[:id])
 end
-
+#New post 
 def new
     @post = Post.new
 end
 
-
+#Process the creation of new post
 def create
     @post = Post.new(post_params)
 
@@ -22,10 +22,27 @@ def create
         render'new'
     end
 end
-
+#Edit the post 
 def edit 
     @post = Post.find(params[:id])
 end
+
+def update
+    @post = Post.find(params[:id])
+
+    if(@post.update(post_params))
+    redirect_to @post
+    else 
+        render'edit', :notice=> 'Your post has been updated'
+    end
+end
+#Delete post 
+
+def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path, :notice => 'Your post has been deleted succesfully'
+end 
 
 private def post_params
     params.require(:post).permit(:title, :body)
